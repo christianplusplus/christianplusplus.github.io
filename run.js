1e40b98be5ac4271d4c856e3a0010a13d3276c41
christianplusplus <48064080+christianplusplus@users.noreply.github.com>
2021-03-14 18:26:49 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,28 +1,23 @@
 pypyjs.ready().then(function() {
-    $(document).ready(function() {
+    // Initialize the widget.
+    var terminal = $('#terminal').jqconsole('', '>>> ');
 
-        var jqconsole = $('#console').jqconsole('Hi\n', '>>>');
-        
-        // Initialize the widget.
-        var terminal = $('#terminal').jqconsole('', '>>> ');
+    // Hook up output streams to write to the console.
+    pypyjs.stdout = pypyjs.stderr = function(data) {
+      terminal.Write(data, 'jqconsole-output');
+    }
 
-        // Hook up output streams to write to the console.
-        pypyjs.stdout = pypyjs.stderr = function(data) {
-          terminal.Write(data, 'jqconsole-output');
-        }
+    // Interact by taking input from the console prompt.
+    pypyjs.repl(function(ps1) {
 
-        // Interact by taking input from the console prompt.
-        pypyjs.repl(function(ps1) {
+      // The argument is ">>> " or "... " depending on REPL state.
+      terminal.SetPromptLabel(ps1);
 
-          // The argument is ">>> " or "... " depending on REPL state.
-          jqconsole.SetPromptLabel(ps1);
-
-          // Return a promise if prompting for input asynchronously.
-          return new Promise(function(resolve, reject) {
-            jqconsole.Prompt(true, function (input) {
-              resolve(input);
-            });
-          });
+      // Return a promise if prompting for input asynchronously.
+      return new Promise(function(resolve, reject) {
+        terminal.Prompt(true, function (input) {
+          resolve(input);
         });
-    })
+      });
+    });
 })
