8c7e8c2496ec212d21f525cede6acdcfdf81844f
christianplusplus <christian.wendlandt.cs@gmail.com>
2021-05-07 14:31:09 -0500

    Update run.js

--- run.js
+++ run.js
@@ -20,7 +20,13 @@ function runBot(){
 
     fetch('ConnectFour/c4blob.py').then(response => {
         response.text().then(text => {
-            Sk.importMainWithBody('c4blob', false, text, true)
+            Sk.misceval.asyncToPromise(suspension =>
+                Sk.importMainWithBody('c4blob', false, text, true)
+            ).then(
+                function(process) {
+                    process(suspension.resume());
+                }
+            );
         });
     });
 }
