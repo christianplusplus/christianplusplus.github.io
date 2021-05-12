f02872b51dbb28223a496afc7324466442b7fa79
christianplusplus <christian.wendlandt.cs@gmail.com>
2021-05-12 16:11:57 -0500

    Update consoleWorker.js

--- consoleWorker.js
+++ consoleWorker.js
@@ -1,17 +1,13 @@
-var jqconsole = $('#console').jqconsole('', '');
+self.importScripts('imports/skulpt.min.js')
+self.importScripts('imports/skulpt-stdlib.js')
 
 var output = function(text) {
     jqconsole.Write(text, 'jqconsole-output');
 }
 
-var input = function(){
-    return new Promise(function(resolve, reject) {
-        jqconsole.Prompt(
-            false,
-            function(text){
-                resolve(text);
-            }
-        );
+var input = function(input){
+    return new Promise(function(resolve) {
+        self.onmessage = fuction
     });
 }
 
@@ -19,8 +15,11 @@ Sk.configure({output:output, inputfun:input, __future__:Sk.python3});
 
 fetch('ConnectFour/c4blob.py').then(response => {
     response.text().then(text =>
-        Sk.misceval.callAsync('skulpt',
-            () => Sk.importMainWithBody('c4blob', false, text, true)
+        Sk.importMainWithBody('<stdin>', false, text)
         )
     );
+});
+
+self.addEventListener('input', function(post) {
+    input(post.data.message)
 });
